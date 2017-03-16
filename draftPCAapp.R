c8804eae208a6a0b5a96228618a1fc9d87690160
ruthtopless <ruth.topless@otago.ac.nz>
2017-03-17 10:00:17 +1300

    introduced download buttons

--- draftPCAapp.R
+++ draftPCAapp.R
@@ -19,11 +19,11 @@ ethnicspecific <- c("African", "East Asian", "Southeast Asian", "East or Southea
 
 
 ##################################
-# Define UI for application that draws a histogram
+# Define UI 
 ui = fluidPage(
-
+  
   theme = shinytheme("spacelab"),
-  headerPanel('PCA clustering'),
+  headerPanel(h1('PCA clustering', align = "center")),
   fluidRow(
     column(width = 4,
            selectInput('xcol', 'X Variable', PCAlist),
@@ -31,27 +31,27 @@ ui = fluidPage(
            selectInput('ethnicclass', 'Choose ethnic groups to include', choices = ethnicspecific, multiple = TRUE ),
            textInput("highlight_subjects", "Type in subject ID to highlight on plot"),
            
-           h4("Points near click"),
-           tableOutput("click_info"),
+           h4("Highlighted subject data"),
+           tableOutput("subjectdata"),
            
-           tableOutput("subjectdata")
+           h4("Points near click in plot1"),
+           tableOutput("click_info")
     ), 
-
+    
     
     column(width = 8,
            plotOutput('plot1', width = "80%", height = "900px",
                       click = clickOpts(id = "plot1_click"),
-                      brush = brushOpts(id = "plot1_brush"),
-                      hover = hoverOpts(id = "plot1_hover")
-           )
-           
-           #downloadButton('downloadData', 'Download')
+                      brush = brushOpts(id = "plot1_brush")
+           ),
+           downloadButton('downloadDataPlot1', 'Download Data from Plot1'),
+           downloadButton('downloadImagePlot1', 'Download Image from Plot1')
     )
   ),
   
   fluidRow(
     column(width = 12,
-           h4("Brushed points"),
+           h4("Brushed points from plot1 which are fed into plot2"),
            dataTableOutput("brush_info")
     )
   ),
@@ -61,22 +61,23 @@ ui = fluidPage(
     column(width = 4,
            selectInput('xcol2', 'X Variable', PCAlist, selected=PCAlist[3]),
            selectInput('ycol2', 'Y Variable', PCAlist, selected=PCAlist[4]),
-           h4("Points near click"),
+           h4("Points near click for plot2"),
            tableOutput("click_info2")
     ),
     column(width = 8,
            plotOutput('plot2', width = "80%", height = "900px",
                       click = clickOpts(id = "plot2_click"),
                       brush = brushOpts(id = "plot2_brush")
-                      #hover = hoverOpts(id = "plot2_hover")
-           )
+           ),
+           downloadButton('downloadDataPlot2', 'Download Data from Plot2'),
+           downloadButton('downloadImagePlot2', 'Download Image from Plot2')
     )
   ),
   
   fluidRow(
     column(width = 12,
-           h4("Brushed points"),
-           tableOutput("brush_info2")
+           h4("Brushed points from plot2"),
+           dataTableOutput("brush_info2")
     )
   )
 )
@@ -86,25 +87,6 @@ ui = fluidPage(
 ####################################################################################################
 server <- function(input, output, session) {
   
-  #function to assign plotting colours to ethinicities
-  colour_eth <- function(x) {
-    unlist(lapply(x, function(x) {switch(x,
-                                         African = "#999999",
-                                         `East Asian`="#E41A1C",
-                                         `Southeast Asian`="#377EB8",
-                                         `South Asian`="#4DAF4A",
-                                         European = "#984EA3",
-                                         Hispanic= "#FF7F00",
-                                         Melanesian="#FFFF33",
-                                         `East Polynesian`="#A65628",
-                                         `West Polynesian`="#F781BF",
-                                         Niuean="#015249",
-                                         Pukapukan="#76323F",
-                                         Polynesian="#000080",
-                                         Unspecified="#49274A"
-    )
-    }))
-  }
   
   # define the colour scale to be used for plotting
   eth_col_scale <- scale_colour_manual(values=c("African" = "#F8766D",
@@ -129,7 +111,7 @@ server <- function(input, output, session) {
     eth <- input$ethnicclass
     if(is.null(eth)) eth <- ethnicspecific  
     return(PCAtestdata %>% 
-             select(SUBJECT, ETH_DESCRIP, which(input$xcol == names(.)), which(input$ycol == names(.)), PCAETHSPECIFIC, SPECCOLOUR) %>% 
+             select(SUBJECT, ETH_DESCRIP, which(input$xcol == names(.)), which(input$ycol == names(.)), PCAETHBROAD, PCAETHSPECIFIC) %>% 
              filter(PCAETHSPECIFIC %in% eth))
   })
   
@@ -140,8 +122,7 @@ server <- function(input, output, session) {
   
   output$subjectdata <- renderTable({ highlight_sub()})
   
-  
-  output$plot1 <- renderPlot({
+  plotInput <- reactive({
     dat <- selectedData()%>% dplyr::rename_(xcol = input$xcol, ycol = input$ycol)
     highlight <- dat %>% filter(SUBJECT %in% input$highlight_subjects)
     dat   %>% 
@@ -157,64 +138,87 @@ server <- function(input, output, session) {
             legend.title = element_text(size = rel(2.0)))+
       geom_point(data = highlight, shape = 18, colour = 'black', size = 5) + 
       geom_text_repel(data = highlight,aes(x = xcol, y = ycol,  label = SUBJECT), colour="black",fontface = 'bold', nudge_x = 0.002, nudge_y = 0.002 )
+  })
     
+  output$plot1 <- renderPlot({ 
+    plotInput()
   })
   
+  output$downloadDataPlot1 <- downloadHandler(
+    filename = function() {paste('plot1-', Sys.Date(), Sys.time(), '.csv', sep='')},
+    content = function(file) {write.csv(selectedData(), file)})
+  output$downloadImagePlot1 <- downloadHandler(
+    filename = function() { paste('plot1-', Sys.Date(), Sys.time(), '.png', sep='') },
+    content = function(file) {ggsave(file,plotInput())}) 
   
-  
-  output$click_info <- renderTable({nearPoints(PCAtestdata[,c("SUBJECT",input$xcol,input$ycol,"ETH_DESCRIP","PCAETHBROAD","PCAETHSPECIFIC")], input$plot1_click, xvar=input$xcol, yvar=input$ycol, addDist = TRUE)
+  output$click_info <- renderTable({
+    dat <- selectedData()
+    nearPoints(dat[,c("SUBJECT",input$xcol,input$ycol,"ETH_DESCRIP","PCAETHBROAD","PCAETHSPECIFIC")], input$plot1_click, xvar=input$xcol, yvar=input$ycol, addDist = FALSE)
   })
-  
   
   plot1brushselected<- reactive({
-    brushedPoints(PCAtestdata[,c("SUBJECT",input$xcol,input$ycol, "ETH_DESCRIP","PCAETHBROAD","PCAETHSPECIFIC")], input$plot1_brush, xvar=input$xcol, yvar=input$ycol)
+    dat <- selectedData()
+    brushedPoints(dat[,c("SUBJECT",input$xcol,input$ycol, "ETH_DESCRIP","PCAETHBROAD","PCAETHSPECIFIC")], input$plot1_brush, xvar=input$xcol, yvar=input$ycol)
   })
   
   output$brush_info <- renderDataTable({ plot1brushselected()},
-   options = list(lengthMenu = list(c(10, 25, 50, -1), c('10','25','50','All')), pageLength = 10)
-   #options = list(scrollY = 200px, scrollCollapse = TRUE, paging = FALSE) #for vertical scrolling
-     )
+                                       #options = list(lengthMenu = list(c(10, 25, 50, -1), c('10','25','50','All')), pageLength = 10, autoWidth=FALSE)
+                                       options = list(scrollY = "300px", scrollCollapse = TRUE, paging = FALSE) #for vertical scrolling
+  )
   
   
   plot1brushselected2<- reactive({
-    brushedPoints(PCAtestdata[,c("SUBJECT",input$xcol,input$ycol,input$xcol2,input$ycol2, "ETH_DESCRIP","PCAETHBROAD","PCAETHSPECIFIC")], input$plot1_brush, xvar=input$xcol, yvar=input$ycol)
+    dat <- selectedData()
+    dat2 <- PCAtestdata %>% filter(SUBJECT %in% dat$SUBJECT)
+    return(brushedPoints(dat2[,c("SUBJECT",input$xcol,input$ycol,input$xcol2,input$ycol2, "ETH_DESCRIP","PCAETHBROAD","PCAETHSPECIFIC")], input$plot1_brush, xvar=input$xcol, yvar=input$ycol))
+  })
+  
+  plotInput2 <- reactive({
+    dat3 <- plot1brushselected2()%>% dplyr::rename_(xcol = input$xcol2, ycol = input$ycol2)
+    highlight <- dat3 %>% filter(SUBJECT %in% input$highlight_subjects)
+    dat3 %>%
+      ggplot(., aes(x = xcol, y = ycol, colour = PCAETHSPECIFIC) ) +
+      geom_point() +
+      xlab(input$xcol2) +
+      ylab(input$ycol2) +
+      eth_col_scale +
+      ylim(c(min(PCAtestdata[, input$ycol2]), max(PCAtestdata[, input$ycol2]) )) +
+      xlim(c(min(PCAtestdata[, input$xcol2]), max(PCAtestdata[, input$xcol2]) )) +
+      theme(legend.position='right',
+            legend.text = element_text(size = rel(1.5)),
+            legend.title = element_text(size = rel(2.0)))+
+      geom_point(data = highlight, shape = 18, colour = 'black', size = 5) +
+      geom_text_repel(data = highlight,aes(x = xcol, y = ycol,  label = SUBJECT), colour="black",fontface = 'bold', nudge_x = 0.002, nudge_y = 0.002 )
   })
   
   output$plot2 <- renderPlot({
-    dat2 <- plot1brushselected2()
-    dat2 %>% dplyr::rename_(xcol = input$xcol2, ycol = input$ycol2) %>%  
-      ggplot(., aes(x = xcol, y = ycol, colour = PCAETHSPECIFIC) ) + 
-      geom_point() + 
-      xlab(input$xcol2) + 
-      ylab(input$ycol2) + 
-      eth_col_scale +              
-      ylim(c(min(PCAtestdata[, input$ycol2]), max(PCAtestdata[, input$ycol2]) )) + 
-      xlim(c(min(PCAtestdata[, input$xcol2]), max(PCAtestdata[, input$xcol2]) ))
-  })
+    plotInput2()
+  })  
+    
+  output$downloadDataPlot2 <- downloadHandler(
+    filename = function() {paste('plot2-', Sys.Date(), Sys.time(), '.csv', sep='')},
+    content = function(file) {write.csv(plot1brushselected2(), file)})
+  
+  output$downloadImagePlot2 <- downloadHandler(
+    filename = function() { paste('plot2-', Sys.Date(), Sys.time(), '.png', sep='') },
+    content = function(file) {ggsave(file,plotInput2())})
   
   output$click_info2 <- renderTable({
-    nearPoints(PCAtestdata[,c("SUBJECT",input$xcol2,input$ycol2,"PCAETHBROAD","PCAETHSPECIFIC")], input$plot2_click, xvar=input$xcol2, yvar=input$ycol2, addDist = FALSE)
+    dat3 <- plot1brushselected2()
+    nearPoints(dat3[,c("SUBJECT",input$xcol2,input$ycol2,"ETH_DESCRIP","PCAETHBROAD","PCAETHSPECIFIC")], input$plot2_click, xvar=input$xcol2, yvar=input$ycol2, addDist = FALSE)
   })
   
-  output$brush_info2 <- renderDataTable({
-    brushedPoints(PCAtestdata[,c("SUBJECT",input$xcol2,input$ycol2,"PCAETHBROAD","PCAETHSPECIFIC")], input$plot2_brush, xvar=input$xcol2, yvar=input$ycol2) 
-      }) 
+  plot2brushselected<- reactive({
+    dat3 <- plot1brushselected2()
+    brushedPoints(dat3[,c("SUBJECT",input$xcol2,input$ycol2,"ETH_DESCRIP","PCAETHBROAD","PCAETHSPECIFIC")], input$plot2_brush, xvar=input$xcol2, yvar=input$ycol2)
+  })
   
   
+  output$brush_info2 <- renderDataTable({ plot2brushselected() },
+                                        options = list(lengthMenu = list(c(10, 25, 50, -1), c('10','25','50','All')), pageLength = 10, autoWidth=FALSE)
+  )
   
 } #end of server
-
-#output$downloadData <- downloadHandler(
-# filename = function() { 
-#  paste(input$dataset, '.csv', sep='') 
-#},
-#content = function(file) {
-#  write.csv(datasetInput(), file)
-#})
-
-
-
-
 
 # Run the application 
 shinyApp(ui = ui, server = server)
