fd77b33e84ca96d941184c40e40d4da614d4971a
ruthtopless <ruth.topless@otago.ac.nz>
2017-03-17 12:36:48 +1300

    finessedthe plot downloads and page layout a bit

--- draftPCAapp.R
+++ draftPCAapp.R
@@ -26,14 +26,16 @@ ui = fluidPage(
   headerPanel(h1('PCA clustering', align = "center")),
   fluidRow(
     column(width = 4,
-           selectInput('xcol', 'X Variable', PCAlist),
-           selectInput('ycol', 'Y Variable', PCAlist, selected=PCAlist[2]),
+           selectInput('xcol', 'X Variable Plot1', PCAlist),
+           selectInput('ycol', 'Y Variable Plot1', PCAlist, selected=PCAlist[2]),
            selectInput('ethnicclass', 'Choose ethnic groups to include', choices = ethnicspecific, multiple = TRUE ),
-           textInput("highlight_subjects", "Type in subject ID to highlight on plot"),
            
+           hr(),
+           textInput("highlight_subjects", "Type in subject ID to highlight on plot"),
            h4("Highlighted subject data"),
            tableOutput("subjectdata"),
            
+           hr(),
            h4("Points near click in plot1"),
            tableOutput("click_info")
     ), 
@@ -44,6 +46,8 @@ ui = fluidPage(
                       click = clickOpts(id = "plot1_click"),
                       brush = brushOpts(id = "plot1_brush")
            ),
+           
+           textInput("filenameplot1", "File name for download of Plot1", placeholder = "Plot1"),
            downloadButton('downloadDataPlot1', 'Download Data from Plot1'),
            downloadButton('downloadImagePlot1', 'Download Image from Plot1')
     )
@@ -56,11 +60,14 @@ ui = fluidPage(
     )
   ),
   
+  hr(),
+  
   
   fluidRow(
     column(width = 4,
-           selectInput('xcol2', 'X Variable', PCAlist, selected=PCAlist[3]),
-           selectInput('ycol2', 'Y Variable', PCAlist, selected=PCAlist[4]),
+           selectInput('xcol2', 'X Variable Plot2', PCAlist, selected=PCAlist[3]),
+           selectInput('ycol2', 'Y Variable Plot2', PCAlist, selected=PCAlist[4]),
+           hr(),
            h4("Points near click for plot2"),
            tableOutput("click_info2")
     ),
@@ -69,13 +76,14 @@ ui = fluidPage(
                       click = clickOpts(id = "plot2_click"),
                       brush = brushOpts(id = "plot2_brush")
            ),
+           textInput("filenameplot2", "File name for download of Plot2", placeholder = "Plot2"),
            downloadButton('downloadDataPlot2', 'Download Data from Plot2'),
            downloadButton('downloadImagePlot2', 'Download Image from Plot2')
     )
   ),
   
   fluidRow(
-    column(width = 12,
+    column(width = 12, 
            h4("Brushed points from plot2"),
            dataTableOutput("brush_info2")
     )
@@ -120,7 +128,7 @@ server <- function(input, output, session) {
     return(PCAtestdata %>% select(SUBJECT, ETH_DESCRIP, which(input$xcol == names(.)), which(input$ycol == names(.)), PCAETHSPECIFIC) %>% filter(SUBJECT %in% subj))
   })
   
-  output$subjectdata <- renderTable({ highlight_sub()})
+  output$subjectdata <- renderTable({ highlight_sub()}, bordered = TRUE)
   
   plotInput <- reactive({
     dat <- selectedData()%>% dplyr::rename_(xcol = input$xcol, ycol = input$ycol)
@@ -133,37 +141,37 @@ server <- function(input, output, session) {
       ylab(input$ycol) + 
       ylim(c(min(PCAtestdata[, input$ycol]), max(PCAtestdata[, input$ycol]) )) + 
       xlim(c(min(PCAtestdata[, input$xcol]), max(PCAtestdata[, input$xcol]) )) +
-      theme(legend.position='right',
+      theme(legend.position='bottom',
             legend.text = element_text(size = rel(1.5)),
-            legend.title = element_text(size = rel(2.0)))+
+            legend.title = element_text(size = rel(1.7)))+
       geom_point(data = highlight, shape = 18, colour = 'black', size = 5) + 
       geom_text_repel(data = highlight,aes(x = xcol, y = ycol,  label = SUBJECT), colour="black",fontface = 'bold', nudge_x = 0.002, nudge_y = 0.002 )
   })
-    
+  
   output$plot1 <- renderPlot({ 
     plotInput()
   })
   
   output$downloadDataPlot1 <- downloadHandler(
-    filename = function() {paste('plot1-', Sys.Date(), Sys.time(), '.csv', sep='')},
+    filename = function() {paste(input$filenameplot1,input$xcol,input$ycol, '.csv', sep='')},
     content = function(file) {write.csv(selectedData(), file)})
   output$downloadImagePlot1 <- downloadHandler(
-    filename = function() { paste('plot1-', Sys.Date(), Sys.time(), '.png', sep='') },
-    content = function(file) {ggsave(file,plotInput())}) 
+    filename = function() { paste(input$filenameplot1,input$xcol,input$ycol, '.png', sep='') },
+    content = function(file) {ggsave(file,plotInput(), device='png', width = 12, height = 12, units = "in", dpi = 300)}) 
   
   output$click_info <- renderTable({
     dat <- selectedData()
-    nearPoints(dat[,c("SUBJECT",input$xcol,input$ycol,"ETH_DESCRIP","PCAETHBROAD","PCAETHSPECIFIC")], input$plot1_click, xvar=input$xcol, yvar=input$ycol, addDist = FALSE)
-  })
+    nearPoints(dat[,c("SUBJECT",input$xcol,input$ycol,"ETH_DESCRIP","PCAETHBROAD","PCAETHSPECIFIC")], input$plot1_click, xvar=input$xcol, yvar=input$ycol, addDist = FALSE)},
+    bordered = TRUE
+  )
   
   plot1brushselected<- reactive({
     dat <- selectedData()
-    brushedPoints(dat[,c("SUBJECT",input$xcol,input$ycol, "ETH_DESCRIP","PCAETHBROAD","PCAETHSPECIFIC")], input$plot1_brush, xvar=input$xcol, yvar=input$ycol)
-  })
+    brushedPoints(dat[,c("SUBJECT",input$xcol,input$ycol, "ETH_DESCRIP","PCAETHBROAD","PCAETHSPECIFIC")], input$plot1_brush, xvar=input$xcol, yvar=input$ycol)})
   
   output$brush_info <- renderDataTable({ plot1brushselected()},
                                        #options = list(lengthMenu = list(c(10, 25, 50, -1), c('10','25','50','All')), pageLength = 10, autoWidth=FALSE)
-                                       options = list(scrollY = "300px", scrollCollapse = TRUE, paging = FALSE) #for vertical scrolling
+                                       options = list(scrollY = "300px", scrollCollapse = TRUE, paging = FALSE, searching=FALSE) #for vertical scrolling
   )
   
   
@@ -186,7 +194,7 @@ server <- function(input, output, session) {
       xlim(c(min(PCAtestdata[, input$xcol2]), max(PCAtestdata[, input$xcol2]) )) +
       theme(legend.position='right',
             legend.text = element_text(size = rel(1.5)),
-            legend.title = element_text(size = rel(2.0)))+
+            legend.title = element_text(size = rel(1.7)))+
       geom_point(data = highlight, shape = 18, colour = 'black', size = 5) +
       geom_text_repel(data = highlight,aes(x = xcol, y = ycol,  label = SUBJECT), colour="black",fontface = 'bold', nudge_x = 0.002, nudge_y = 0.002 )
   })
@@ -194,19 +202,20 @@ server <- function(input, output, session) {
   output$plot2 <- renderPlot({
     plotInput2()
   })  
-    
+  
   output$downloadDataPlot2 <- downloadHandler(
-    filename = function() {paste('plot2-', Sys.Date(), Sys.time(), '.csv', sep='')},
+    filename = function() {paste(input$filenameplot2,input$xcol2,input$ycol2, '.csv', sep='')},
     content = function(file) {write.csv(plot1brushselected2(), file)})
   
   output$downloadImagePlot2 <- downloadHandler(
-    filename = function() { paste('plot2-', Sys.Date(), Sys.time(), '.png', sep='') },
-    content = function(file) {ggsave(file,plotInput2())})
+    filename = function() { paste(input$filenameplot2,input$xcol2,input$ycol2, '.png', sep='') },
+    content = function(file) {ggsave(file,plotInput2(), device='png', width = 12, height = 12, units = "in", dpi = 300)})
+  
   
   output$click_info2 <- renderTable({
     dat3 <- plot1brushselected2()
     nearPoints(dat3[,c("SUBJECT",input$xcol2,input$ycol2,"ETH_DESCRIP","PCAETHBROAD","PCAETHSPECIFIC")], input$plot2_click, xvar=input$xcol2, yvar=input$ycol2, addDist = FALSE)
-  })
+  }, bordered = TRUE)
   
   plot2brushselected<- reactive({
     dat3 <- plot1brushselected2()
